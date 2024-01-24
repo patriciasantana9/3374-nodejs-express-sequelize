3bf24eaf8bcd0c0859db234658b671908889de72
Patrícia Santana <patriciasan3@gmail.com>
2024-01-24 17:35:11 -0300

    correção dos models

--- src/models/categoria.js
+++ src/models/categoria.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Categoria',
+    tableName: 'categorias',
   });
   return Categoria;
 };
--- src/models/curso.js
+++ src/models/curso.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Curso',
+    tableName: 'cursos',
   });
   return Curso;
 };
