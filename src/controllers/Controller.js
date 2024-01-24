193f5925eebeb1e1a308e2b7e710408349772e6e
Patrícia Santana <patriciasan3@gmail.com>
2024-01-24 17:24:31 -0300

    métodos CRUD

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -3,6 +3,7 @@ class Controller {
     this.entidadeService = entidadeService;
   }
 
+  
   //pegar o registro de qualquer controller
   async pegaTodos(req, res){
     try {
@@ -11,6 +12,51 @@ class Controller {
       return res.status(200).json(listaDeRegistro);  
     } catch(error) {
       //erro
+    }
+  }
+
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async criaNovo(req, res) {
+    const dadosParaCriacao = req.body;
+    try {
+      const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+      return res.status(200).json(novoRegistroCriado);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async atualiza(req, res){
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+
+    try { /* isUpdated */
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      if(!foiAtualizado) {
+        return res.status(400).json({ mensagem: 'O registro não foi atualizado.'});  
+      }
+      return res.status(200).json({ mensagem: 'Registro atualizado com sucesso.'});  
+    } catch (error) {
+      //error
+    }
+  }
+
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.excluiRegistro(Number(id));
+      return res.status(200).json({ mensagem: `id ${id} deletado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
   }
 }
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -7,5 +7,6 @@ const router = Router();
 
 //rotaGet(caminho, acao.metodoDaAcao)
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
 
 module.exports = router;
